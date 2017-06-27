b7ec23e6b2032041b4ce530cb5c25bf1eab7ab00
Krati Ahuja <kratiahuja@gmail.com>
2017-06-26 18:23:19 -0700

    Prepare for FastBoot 1.0

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var existsSync = require('exists-sync');
+var fastbootTransform = require('fastboot-transform');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-cli-bootstrap-datepicker',
 
   included: function(app) {
     this._super.included(app);
 
-    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-      app.import(app.bowerDirectory + '/bootstrap-datepicker/dist/js/bootstrap-datepicker.js');
-      app.import(app.bowerDirectory + '/bootstrap-datepicker/dist/css/bootstrap-datepicker.css');
+    app.import('vendor/bootstrap-datepicker/bootstrap-datepicker.js');
+    app.import(app.bowerDirectory + '/bootstrap-datepicker/dist/css/bootstrap-datepicker.css');
+  },
+
+  treeForVendor(tree) {
+    var trees = [];
+
+    if (tree) {
+      trees.push(tree);
     }
+
+    var bootstrapDatepickerPath = path.join(this.project.root, this.app.bowerDirectory, 'bootstrap-datepicker', 'dist', 'js');
+
+    if (existsSync(bootstrapDatepickerPath)) {
+      var bootstrapTree = fastbootTransform(new Funnel(bootstrapDatepickerPath, {
+        files: ['bootstrap-datepicker.js'],
+        destDir: 'bootstrap-datepicker'
+      }));
+
+      trees.push(bootstrapTree);
+    }
+
+    return new MergeTrees(trees);
   }
 };
 
